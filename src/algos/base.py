c3f983ea671734e4d336b6a378f0e200712de986
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-19 11:09:57 +0100

    @justify decorator added to dynamically duplicate an algorithm and add a justifying version to the table.

--- src/algos/base.py
+++ src/algos/base.py
@@ -26,3 +26,41 @@ def algo(doc=""):
         register(func, doc)
         return func
     return wrapper
+
+def justify_line(line, width): # TODO test me
+    """
+    Justify a line. 'line' is the list of words for this line, and 'width' the
+    maximum output width.
+    """
+    wcount = len(line)
+    length = wcount + sum(map(len, line)) - 1
+    if wcount < 2 or length >= width:
+        return line
+
+    lastword = line.pop()
+    trailing = width-length-1
+    spaces, onemore = divmod(trailing, wcount)
+    sp  = ' '*spaces
+    sp2 = sp+' '
+    line.append(lastword)
+    line[:-1] = map(lambda wi: wi[1]+(sp2 if wi[0] < onemore else sp), \
+            enumerate(line[:-1]))
+    return line
+
+def justify(func):
+    """
+    A decorator for algorithms functions to yield justified lines. This
+    slightly increase the run time but avoid a lot of code duplication. It
+    adds O(n) to the overall complexity of the algorithm, but since we can't
+    have lower complexities it's not _that_ annoying.
+    """
+    def wrapper(txt, width):
+        for line_words in func(txt, width):
+            yield justify_line(line_words, width)
+
+    name = func.__name__
+    wrapper.__name__ = name + '_justify'
+    name = name.replace('_', '-')
+    wrapper.__doc__  = '\n    See \'%s\'.\n' % name
+    register(wrapper, 'Same as %s with justifying' % name)
+    return func
--- src/algos/greedy.py
+++ src/algos/greedy.py
@@ -1,33 +1,14 @@
 # -*- coding: UTF-8 -*-
-from .base import algo
+from .base import algo, justify
 
-def justify(line, length, width):
-    """
-    Justify a line. 'line' is the list of words for this line, 'length' is its
-    total width with one space between each pair of words, and 'width' the
-    maximum output width.
-    """
-    wcount = len(line)
-    if wcount < 2 or length >= width:
-        return ' '.join(line)
-
-    lastword = line.pop()
-    trailing = width-length-1
-    spaces, onemore = divmod(trailing, wcount)
-    sp  = ' '*spaces
-    sp2 = sp+' '
-    line = map(lambda wi: wi[1]+(sp2 if wi[0] < onemore else sp), \
-            enumerate(line))
-    return ' '.join(line) + ' ' + lastword
-
-
+@justify
 @algo("The most basic greedy algorithm")
 def naive_greedy(words, width):
     """
     This algorithm is the most basic one. It adds words to a line until it
     reaches the end of it, then continues on the next line.
-    Its time complexity is linear and its memory one is constant, assuming
-    the storage of the text is ignored.
+    Its time complexity is O(n) and its memory one is O(k), where n is the
+    length of the text and k the maximum width.
     """
     line = []
     # each word uses its length plus one space. To avoid exceptions, we assume
@@ -41,29 +22,7 @@ def naive_greedy(words, width):
             line.append(word)
             w -= lw+1
         else:
-            yield ' '.join(line)
+            yield line
             line = [word]
             w = width-lw-1
-
-
-@algo("Naive greedy with justifying")
-def naive_greedy_justify(words, width):
-    """
-    See naive-greedy for the details.
-    """
-    line = []
-    linelen = 0
-    width += 1
-    w = width
-    for word in words:
-        lw = len(word)
-        if lw+1 <= w or w == width:
-            line.append(word)
-            linelen += lw + 1
-            w -= lw + 1
-        else:
-            yield justify(line, linelen-1, width)
-            line = [word]
-            linelen = lw + 1
-            w = width - linelen
 
