b8e1ce415c9fdb97c49abf60259f4779ccd452fd
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-20 11:51:35 +0100

    Tests added for justify_line

--- tests/test_algo_base.py
+++ tests/test_algo_base.py
@@ -10,6 +10,9 @@ class TestAlgosBase(unittest.TestCase):
         def f():
             pass
         self.fun = f
+        self.l = ['foo', 'bar']
+
+    # - register - #
 
     def test_register(self):
         self.assertEqual(len(b.ALGOS.keys()), 0)
@@ -26,8 +29,43 @@ class TestAlgosBase(unittest.TestCase):
         b.register(self.fun, doc)
         self.assertEqual(b.ALGOS['f'][1], doc)
 
+    # - get_algos - #
+
     def test_get_algo(self):
         doc = 'foo'
         b.register(self.fun, doc)
         self.assertEqual(b.get_algos()['f'][1], doc)
 
+    # - justify_line - #
+
+    def test_justify_empty_line(self):
+        self.assertEqual(b.justify_line([],  0), [])
+        self.assertEqual(b.justify_line([], 42), [])
+
+    def test_justify_negative_width(self):
+        self.assertEqual(b.justify_line(self.l, -42), self.l)
+
+    def test_justify_no_width(self):
+        self.assertEqual(b.justify_line(self.l, 0), self.l)
+
+    def test_justify_too_small_width(self):
+        self.assertEqual(b.justify_line(self.l, 1), self.l)
+        self.assertEqual(b.justify_line(self.l, 6), self.l)
+
+    def test_justify_exact_width(self):
+        self.assertEqual(b.justify_line(self.l, 7), self.l)
+
+    def test_justify_one_more_space(self):
+        l = self.l
+        l[0] += ' '
+        self.assertEqual(b.justify_line(self.l, 8), l)
+
+    def test_justify_one_space_per_word(self):
+        l = self.l
+        l[0] += '  '
+        self.assertEqual(b.justify_line(self.l, 9), l)
+
+    def test_justify(self):
+        l1 = ['foo', 'x', 'bar', 'zzzz']
+        l2 = [l1[0] + '  ', l1[1] + '  ', l1[2] + ' ', l1[3]]
+        self.assertEqual(b.justify_line(l1, 19), l2)
