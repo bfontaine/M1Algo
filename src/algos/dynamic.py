9af30573e790c3349d0baa17329aee130ff1522c
david <galichet.david@gmail.com>
2013-11-25 17:05:51 +0100

    optimize word wrap function and add two methode with function (square and cube).

--- src/algos/dynamic.py
+++ src/algos/dynamic.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 from .base import algo
 
+def carre(n):
+	return n*n
+
+def cube(n):
+	return n*n*n
+
 def retsoluce(words, lines, n):
 	if (lines[n] == 1):
 		line = []
@@ -13,19 +19,8 @@ def retsoluce(words, lines, n):
 		line = line + [words[i-1]]
 	return (para + [line],[])
 
-def print_s(p,n):
-	if p[n] == 1:
-		k = 1
-	else:
-		k = print_s(p,p[n]-1) + 1
-	print("l nb ",k,"from w nb : ",p[n]," to ",n)
-	return k
-
-def solvesWordsWrap(l, n , W):
+def WordsWrap(len_word, n , Width, func = carre):
 	INF = 100000
-	# extra[i][j] wil have numbre of extra space 
-	# if words from i to j in a put a single line
-	extra = [[0 for i in range(0,(n+1))] for j in range(0,(n+1))]
 	# lc[i][j] will have cost of a line which has words from i to j
 	lc = [[0 for i in range(0,(n+1))] for j in range(0,(n+1))]
 	# c[i] will have total cost of optimal arrangement of words 
@@ -33,24 +28,23 @@ def solvesWordsWrap(l, n , W):
 	c = [0 for i in range(0,n+1)]
 	# line is used to return solution
 	line = [0 for i in range(0,n+1)]
-	#calculate extra spaces in a single line.  The value extra[i][j]
+	# calculate extra spaces in a single line.  The value lc[i][j]
 	# indicates extra spaces if words from word number i to j are
 	# placed in a single line
 	for i in range(1,n+1):
-		extra[i][i] = W - l[i-1]
+		lc[i][i] = Width - len_word[i-1]
 		for j in range(i+1,n+1):
-			extra[i][j] = extra[i][j-1] - l[j-1] - 1
-	# Calculate line cost corresponding to the above calculated extra
-	# spaces. The value lc[i][j] indicates cost of putting words from
-	# word number i to j in a single line
-	for i in range(1,n+1):
+			lc[i][j] = lc[i][j-1] - len_word[j-1] - 1
+		# Calculate line cost corresponding to the above calculated extra spaces.
+		# The value lc[i][j] indicates cost of putting words from word 
+		# number i to j in a single line
 		for j in range(i,n+1):
-			if (extra[i][j] < 0):
+			if (lc[i][j] < 0):
 				lc[i][j] = INF
-			elif ((j == n) and (extra[i][j] <= 0)):
+			elif ((j == n) and (lc[i][j] <= 0)):
 				lc[i][j] = 0
 			else :
-				lc[i][j] = extra[i][j] * extra[i][j]
+				lc[i][j] = func(lc[i][j])
 	# Calculate line cost corresponding to the above calculated extra
 	# spaces. The value lc[i][j] indicates cost of putting words from
 	# word number i to j in a single line
@@ -65,13 +59,20 @@ def solvesWordsWrap(l, n , W):
 				line[j] = i
 	return line
 
-
-@algo("A dynamic programming")
-def dynamic(words , width):
+@algo("A dynamic programming use methode word wrap to knuth with function square")
+def dynamic_carre(words , width):
 	words=[w for w in words]
 	len_words = len(words)
 	l = [len(w) for w in words]
-	lines = solvesWordsWrap(l , len_words, width)
+	lines = WordsWrap(l , len_words, width, carre)
 	(para,line_words) = retsoluce(words, lines, len_words)
-	print_s(lines, len_words)
+	return para
+
+@algo("A dynamic programming use methode word wrap to knuth with function cube")
+def dynamic_cube(words , width):
+	words=[w for w in words]
+	len_words = len(words)
+	l = [len(w) for w in words]
+	lines = WordsWrap(l , len_words, width, cube)
+	(para,line_words) = retsoluce(words, lines, len_words)
 	return para
