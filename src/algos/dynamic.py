1a27a309ad7cfdc2cb7f1773b0f01f763789fab2
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-26 17:21:44 +0100

    syntax.

--- src/algos/dynamic.py
+++ src/algos/dynamic.py
@@ -17,7 +17,7 @@ def retsoluce(words, lines, n): # TODO rename
     para.append(line)
     return (para, [])
 
-# TODO rename the function 'words_wrap' is too vague
+# TODO rename the function, 'words_wrap' is too vague
 def words_wrap(len_word, n, width, exp=2):
     exp = max(1, exp)
     # lc[i][j] will have cost of a line which has words from i to j
@@ -34,49 +34,47 @@ def words_wrap(len_word, n, width, exp=2):
         #    if words from word number i to j are
         #    placed in a single line
         lc[i][i] = width - len_word[i-1]
-        for j in range(i+1,n+1):
+        for j in range(i+1, n+1):
             lc[i][j] = lc[i][j-1] - len_word[j-1] - 1
         # Calculate line cost corresponding to the above calculated extra spaces.
         # The value lc[i][j] indicates cost of putting words 
         #    from word number i to j in a single line
-        for j in range(i,n+1):
-            if (lc[i][j] < 0):
+        for j in range(i, n+1):
+            if lc[i][j] < 0:
                 lc[i][j] = INF
-            elif ((j == n) and (lc[i][j] <= 0)):
+            elif j == n and lc[i][j] == 0:
                 lc[i][j] = 0
-            else :
+            else:
                 lc[i][j] = pow(lc[i][j], exp)
     # Calculate minimum cost and find minimum cost arrangement.
     # The value c[j] indicates optimized cost to arrange words
     #    from word number 1 to j.
-    for j in range(1,n+1):
-        c[j]= INF
-        for i in range(1,j+1):
-            if((c[i-1] != INF)\
-                and (lc[i][j] != INF )\
-                and  ((c[i-1] + lc[i][j]) < c[j])):
+    for j in range(1, n+1):
+        c[j] = INF
+        for i in range(1, j+1):
+            if c[i-1] != INF and lc[i][j] != INF and c[i-1] + lc[i][j] < c[j]:
                 c[j] = c[i-1] + lc[i][j]
                 line[j] = i
     return line
 
 @algo("A dynamic programming use methode word wrap to knuth with function square")
-def dynamic_carre(words , width):
+def dynamic_carre(words, width):
     words=[w for w in words]
     len_words = len(words)
     if (len_words == 0):
         return [[]]
     l = [len(w) for w in words]
-    lines = words_wrap(l , len_words, width, 2)
+    lines = words_wrap(l, len_words, width, 2)
     (para,line_words) = retsoluce(words, lines, len_words)
     return para
 
 @algo("A dynamic programming use methode word wrap to knuth with function cube")
-def dynamic_cube(words , width):
+def dynamic_cube(words, width):
     words=[w for w in words]
     len_words = len(words)
     if (len_words == 0):
         return [[]]
     l = [len(w) for w in words]
-    lines = words_wrap(l , len_words, width, 3)
-    (para,line_words) = retsoluce(words, lines, len_words)
+    lines = words_wrap(l, len_words, width, 3)
+    para, line_words = retsoluce(words, lines, len_words)
     return para
